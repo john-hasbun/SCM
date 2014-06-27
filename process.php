33b7c1f4ceb8eba38cbac2f6668f7f33037fcf34
Jonathan Hasbun <jhasbun@pdx.edu>
2014-06-27 12:02:22 -0700

    Version 0.1, this is the base structure.

---
+++ process.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SCM</title>
+    <link rel="stylesheet" href="scm.css">
+    <meta charset="utf-8">
+    <meta name="description" content="This is an SEO Content Manager. Upload your homepage and the webapp will create as many pages as necessary.">
+    <meta name="author" content="Jonathan Hasbun">
+</head>
+<body>
+    <!-- CONTAINER -->
+    <div id="container">
+
+        <!-- HEADER -->
+        <header>
+            <p id="banner">The title will go here.</p>
+        </header>
+
+        <!-- CONTENT -->
+        <div id="body">
+            <section id="content">
+				<?php
+
+				?>
+			</section>
+		</div>
+
+		<!-- FOOTER -->
+		<footer>
+			<p>The footer will go here</p>
+		</footer>
+	</div>
+</body>
+</html>
